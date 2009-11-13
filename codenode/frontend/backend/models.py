e9f14f5727398a46ee90391b5e6836075f452e1c
James Casbon <james.casbon@popgentech.com>
2009-11-13 19:40:44 +0000

    installing from choice of configured python

--- codenode/frontend/backend/models.py
+++ codenode/frontend/backend/models.py
@@ -7,6 +7,9 @@ from django.contrib.auth.models import User
 
 from codenode.frontend.notebook.models import Notebook
 
+# this should be imported from settings, but that isn't avaliable until a codenode is actually created
+VE_PATH = '/tmp'
+
 
 class BackendServer(models.Model):
     """This currently assumes the server is accessed via http (xmlrpc) by
@@ -30,31 +33,60 @@ class EngineType(models.Model):
     def __unicode__(self):
         return u"%s Engine Type " % (self.name,)
         
+class Python(models.Model):
+    """A python installation. Must have virtualenv installed. """
+    executable = models.CharField(max_length=255)
+    version = models.CharField(max_length=10)
+        
 class VirtualenvEngine(models.Model):
     """A managed python engine in a virtualenv.
+    
+    Note we don't store the location of the files or executables here as they could be different 
+    on a backend server.  Instead they are calculated from the current settings.
     """
+    python = models.ForeignKey(Python)
     name = models.CharField(max_length=100, primary_key=True)
-    python_path = models.CharField(max_length=255, editable=False)    
+    
+    @property
+    def directory(self):
+        return os.path.join(VE_PATH, self.name)
+    
+    @property 
+    def executable(self):
+        return os.path.join(self.directory, 'bin', 'python')
+        
+    @property
+    def site_packages(self):
+        # must be a better way from using site to return the correct directory 
+        return os.path.join(self.directory, 'lib', 'python' + self.python.version[:3], 'site-packages')
+    
+    def installed(self):
+        return os.path.exists(self.directory) and os.path.exists(self.executable)
+    
+    virtualenv_incantation = "import sys,pkg_resources; sys.exit( \
+        pkg_resources.load_entry_point('virtualenv', 'console_scripts', 'virtualenv')())"
+    
+    codenode_code = os.path.abspath(os.path.join(os.path.basename(__file__), '..','..','..'))
     
     def __unicode__(self):
         return u"Virtualenv Engine %s" % (self.name,)
-        
-    def save(self, *args, **kwargs):
-        """Intercept save to create the virtalenv and set the python_path.
-        Could be done with a signal, or django admin action?
-        """
-        self._create_ve()
-        super(VirtualenvEngine, self).save(*args, **kwargs)
     
-    def _create_ve(self):
-        
-        target = '/tmp/%s' % self.name
+    def create(self):
+
+        # Create the virtualenv.
+        # You cannot access the virtualenv API directly unless running from a 'real' python
+        # so invoke the script instead
         subprocess.check_call(
-            ['/opt/local/Library/Frameworks/Python.framework/Versions/2.5/bin/python', 
-            '/opt/local/bin/virtualenv', 
-            target]
+            [self.python.executable, 
+                '-c', self.virtualenv_incantation, 
+                '--no-site-packages', self.directory], 
+            stdout=subprocess.PIPE, 
+            stderr=subprocess.PIPE
         )
-        
+        # TODO: check for errors in the output 
+
+        # Link in the codenode code.       
+        os.symlink(self.codenode_code, os.path.join(self.site_packages, 'codenode'))
 
 class NotebookBackendRecord(models.Model):
     """
--- codenode/frontend/backend/tests.py
+++ codenode/frontend/backend/tests.py
@@ -1,13 +1,31 @@
+import os
+import subprocess
+import shutil
+
 from django.test import TestCase
 
 import models as m
 
 class TestBackendModels(TestCase):
+        
+    def tearDown(self):
+        if os.path.exists('/tmp/unit_test'):
+            shutil.rmtree('/tmp/unit_test')
     
     def test_create_VirtualenvEngine(self):
         
-        ve = m.VirtualenvEngine(name='unit_test')
+        # get the system python version so that this can work on other peoples machines
+        version = subprocess.Popen(['/usr/bin/python', '--version'], stderr=subprocess.PIPE).\
+            stderr.read().split()[1].rstrip()
+            
+        py = m.Python(version=version, executable='/usr/bin/python')
+        py.save()
         
+        ve = m.VirtualenvEngine(python=py, name='unit_test')
         ve.save()
         
-        1/0
+        assert not ve.installed()
+        ve.create()
+        assert ve.installed()
+        
+        
