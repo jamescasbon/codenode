ce400d17f976d0d877bef1c45da00d0ab6883a48
James Casbon <james.casbon@popgentech.com>
2009-11-13 20:18:11 +0000

    better test for ve engine

--- codenode/frontend/backend/tests.py
+++ codenode/frontend/backend/tests.py
@@ -7,6 +7,13 @@ from django.test import TestCase
 import models as m
 
 class TestBackendModels(TestCase):
+    
+    # mark the test as slow so you can exclude it with ./test.sh -a \!slow
+    slow = True
+    
+    def setUp(self):
+        if os.path.exists('/tmp/unit_test'):
+            shutil.rmtree('/tmp/unit_test')
         
     def tearDown(self):
         if os.path.exists('/tmp/unit_test'):
@@ -24,8 +31,12 @@ class TestBackendModels(TestCase):
         ve = m.VirtualenvEngine(python=py, name='unit_test')
         ve.save()
         
+        # try creating it 
         assert not ve.installed()
         ve.create()
         assert ve.installed()
+        
+        # get is as engine configuration
+        assert ve.as_engine_configuration()
         
         
