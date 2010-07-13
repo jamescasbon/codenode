bb22cd572f115bcffbee44704614d3eff4b48825
James Casbon <james.casbon@popgentech.com>
2010-07-13 22:23:54 +0100

    adjust text area size after content is loaded

--- codenode/frontend/static/js/TreeBranch.js
+++ codenode/frontend/static/js/TreeBranch.js
@@ -179,7 +179,6 @@ Notebook.TreeBranch.setOutputCell = function(outputnode, inputid) {
     $('img.outputimage').one('load', oid, function(e) {
             $('#'+e.data)[0].adjustTextarea();
             });
-    outputnode.adjustTextarea();
     //outputnode.saved = true;
     if ($('#auxdisplay').html() == 'spawner') {
         outputnode.spawnerNode().setFocus();
@@ -211,6 +210,7 @@ Notebook.TreeBranch.spawnOutputCellNode = function(id, cellstyle, content, count
         self.setOutputCell(node, id);
         // set the content after appening to the DOM to please mathjax
         node.content(content);
+        node.adjustTextarea();
         return node;
     } else {
         self.setNullOutputCell(id);
